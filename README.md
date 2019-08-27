ca620de42eb1b43827864b53ff87cf07d370f331
Blomqvist Christer <christer.blomqvist@volvo.com>
2019-08-27 19:23:20 +0200

    Update readme and add setup.bat to set up files and symlinks

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # symlink-test
-Test repo for git symlinks in Windows
+
+Test repo for git symlinks in Windows 10
+
+The file `setup.bat` generates all but the `README.md` and the `LICENSE` file in this repo.
+
+A commit has been made after that script was run.
+
+## Prereqs
+
+Before cloning this repo make sure to:
+
+* Enable developer mode in Windows 10
+* Enable symlinks in Git: `git config --global core.symlinks true`
---
+++ setup.bat
@@ -0,0 +1,60 @@
+@echo off
+REM ---------------------------------------------------------------------------
+REM Creates the basic structure with symlinks
+REM ---------------------------------------------------------------------------
+
+call :cleanup
+call :create_libs
+call :create_global_script_link
+call :create_units
+echo "Done"
+exit /B %ERRORLEVEL%
+
+:cleanup
+@echo === Cleaning up old stuff if any... ===
+DEL /F/Q/S libs\*.*
+rmdir /Q/S libs
+DEL /F/Q/S unit1\*.*
+rmdir /Q/S unit1
+DEL /F/Q/S unit2\*.*
+rmdir /Q/S unit2
+del global_script.bat
+@echo === Clean up done! ===
+exit /B 0
+
+:create_libs
+mkdir libs
+call :create_lib 1
+call :create_lib 2
+echo @echo script %%0 > libs\script.bat
+echo @pause >> libs\script.bat
+echo @echo global script %%0 > libs\global_script.bat
+echo @pause >> libs\global_script.bat
+exit /B 0
+
+:create_lib
+echo Creating libs\lib%1
+mkdir libs\lib%1
+echo @echo libfile%1 %%0 > libs\lib%1\libfile.bat
+echo @pause >> libs\lib%1\libfile.bat
+exit /B 0
+
+:create_units
+call :create_unit 1
+call :create_unit 2
+exit /B 0
+
+:create_unit
+echo Creating unit%1
+mkdir unit%1
+mkdir unit%1\app
+echo Creating unit%1\app\appfile%1.bat
+echo @echo app %1 %%0 > unit%1\app\appfile.bat
+echo @pause >> unit%1\app\appfile.bat
+mklink /D unit%1\lib ..\libs\lib%1
+mklink unit%1\script.bat ..\libs\script.bat
+exit /B 0
+
+:create_global_script_link
+mklink global_script.bat libs\global_script.bat
+exit /B 0
